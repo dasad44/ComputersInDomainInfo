5c26749188657aa47df94621b1f40429d1512de4
dasad44 <dasad33_79@o2.pl>
2020-07-15 12:55:28 +0200

    Start

---
+++ ComputersInDomainInfo/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ComputersInDomainInfo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ManagementScope scope = null;
+            ConnectionOptions options = new ConnectionOptions();
+            options.Impersonation = System.Management.ImpersonationLevel.Impersonate;
+            options.Username = "Administrator";
+            options.Password = "777Dasad777";
+            options.Authority = "ntlmdomain:edu.pl";
+
+
+            scope = new ManagementScope("\\\\192.168.0.3\\root\\cimv2", options);
+            scope.Connect();
+
+            ObjectQuery query = new ObjectQuery("Select Caption FROM Win32_OperatingSystem");
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, query);
+
+            ManagementObjectCollection queryCollection = searcher.Get();
+            foreach (ManagementObject m in queryCollection)
+            {
+                MessageBox.Show(m["Caption"].ToString());
+            }
+        }
+    }
+}
